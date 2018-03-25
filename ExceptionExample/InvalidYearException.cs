0ee0e4aa71645f59a47215077921c6ba0c9d39d7
Patryk Bielecki <p.patrykbielecki@gmail.com>
2018-03-25 19:47:44 +0200

    Exception example

---
+++ ExceptionExample/InvalidYearException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionExample
+{
+    internal class InvalidYearException : Exception
+    {
+        public InvalidYearException()
+            : base("User typed invalid year") { }
+    }
+}
---
+++ ExceptionExample/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionExample
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine("Please type your year of birth");
+            var year = Console.ReadLine();
+
+            try
+            {
+                if (year?.Length != 4 )
+                {
+                    throw new InvalidYearException();
+                }
+
+                Console.WriteLine($"You was born in {year}");
+            }
+            catch (InvalidYearException ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                Console.ReadKey();
+            }
+        }
+    }
+}
